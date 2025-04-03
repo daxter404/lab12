814b66e875c8edc795ce6d7c59529ab652f9b1bc
daxter404 <147585619+daxter404@users.noreply.github.com>
2025-04-03 23:07:42 +0500

    Delete lab12 2 directory

--- lab12 2/src/main/java/com/example/lab12/Product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab12.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
